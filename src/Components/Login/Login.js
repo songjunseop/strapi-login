7f6bd866c714b748d3a2893f97ac94d447da24c3
junseopsong <jun80444@wordvice.com>
2022-07-22 13:49:10 +0900

    20220722

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,58 @@
+import React, {useState, useEffect} from "react";
+import './Login.css';
+import axios from "axios";
+
+function Login() {
+    const [inputId, setInputId] = useState('');
+    const [inputPw, setInputPw] = useState('');
+
+    const handleInputId = (e) => {
+        setInputId(e.target.value)
+    }
+    const handleInputPw = (e) => {
+        setInputPw(e.target.value)
+    }
+
+    const onClickLogin = () => {
+
+        axios
+            .post('http://localhost:1337/auth/local', {
+                'identifier': inputId,
+                'password': inputPw,
+            })
+            .then((response) => {
+                console.log('User profile', response.data.user);
+                console.log('User token', response.data.jwt);
+            })
+            .catch((error) => {
+                console.log('An error occurred:', error.response);
+            });
+    }
+
+    useEffect(() => {
+        axios.get('http://localhost:1337/users')
+            .then(res => console.log(res.data))
+            .catch()
+    }, [])
+
+    return (
+        <div className='wrap'>
+            <div className='login'>
+                <h2>Login</h2>
+                <div>
+                    <label htmlFor="identifier">ID : </label>
+                    <input type="text" name='identifier' value={inputId} onChange={handleInputId}/>
+                </div>
+                <div>
+                    <label htmlFor="password">PW : </label>
+                    <input type="password" name='password' value={inputPw} onChange={handleInputPw}/>
+                </div>
+                <div>
+                    <button type='button' onClick={onClickLogin}>Login</button>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+export default Login;
